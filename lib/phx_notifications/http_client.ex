947f7a933ccda3c3bf5718f54f2a55fc71c027b6
tato <61908291+tato-gh@users.noreply.github.com>
2023-08-04 14:49:59 +0900

    feat: headersにUrgencyを追加

--- lib/phx_notifications/http_client.ex
+++ lib/phx_notifications/http_client.ex
@@ -1,6 +1,8 @@
 defmodule PhxNotifications.HttpClient do
   # Call from WebPushEncryption
   def post(endpoint, body, headers, _options) do
+    # for iOS 16.4
+    headers = Map.put(headers, "Urgency", "normal")
     req = Req.new(url: endpoint, headers: headers, body: body)
 
     Req.post!(req)
